bf4592f14ea05dbc7b68984f30467c086777b42a
Sankalpana8 <thushani.sankalpana@gmail.com>
2023-09-18 08:38:15 +0530

    GRA-171 get data

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -33,12 +33,12 @@ export const deleteHotel = async (req, res, next) => {
     next(err);
   }
 };
-// export const getHotel = async (req, res, next) => {
-//   try {
-//     const hotel = await Hotel.findById(req.params.id);
-//     res.status(200).json(hotel);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
+export const getHotel = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    next(err);
+  }
+};
 
