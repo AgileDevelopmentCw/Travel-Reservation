d4ad3e7c48707b71baabc756828034f2b558dfed
Sankalpana8 <thushani.sankalpana@gmail.com>
2023-09-19 12:08:43 +0530

    GRA-174 intergate API

--- api/controllers/hotelBooking.js
+++ api/controllers/hotelBooking.js
@@ -12,27 +12,27 @@ export const updateBooking = async (req,res,next)=>{
     next(err);
   }
 }
-// export const deleteBooking = async (req,res,next)=>{
-//   try {
-//     await Booking.findByIdAndDelete(req.params.id);
-//     res.status(200).json("Booking has been deleted.");
-//   } catch (err) {
-//     next(err);
-//   }
-// }
-// export const getBooking = async (req,res,next)=>{
-//   try {
-//     const Booking = await Booking.findById(req.params.id);
-//     res.status(200).json(Booking);
-//   } catch (err) {
-//     next(err);
-//   }
-// }
-// export const getBookings = async (req,res,next)=>{
-//   try {
-//     const Bookings = await Booking.find();
-//     res.status(200).json(Bookings);
-//   } catch (err) {
-//     next(err);
-//   }
-// }
+export const deleteBooking = async (req,res,next)=>{
+  try {
+    await Booking.findByIdAndDelete(req.params.id);
+    res.status(200).json("Booking has been deleted.");
+  } catch (err) {
+    next(err);
+  }
+}
+export const getBooking = async (req,res,next)=>{
+  try {
+    const Booking = await Booking.findById(req.params.id);
+    res.status(200).json(Booking);
+  } catch (err) {
+    next(err);
+  }
+}
+export const getBookings = async (req,res,next)=>{
+  try {
+    const Bookings = await Booking.find();
+    res.status(200).json(Bookings);
+  } catch (err) {
+    next(err);
+  }
+}
