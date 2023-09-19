b2fa260f05b27e411068ace2439a49841600f8f7
Sankalpana8 <thushani.sankalpana@gmail.com>
2023-09-19 09:26:59 +0530

    GRA-173 updateBooking

---
+++ api/controllers/hotel.js
@@ -0,0 +1,38 @@
+import Booking from "../models/Booking.js";
+
+export const updateBooking = async (req,res,next)=>{
+  try {
+    const updatedBooking = await Booking.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedBooking);
+  } catch (err) {
+    next(err);
+  }
+}
+// export const deleteBooking = async (req,res,next)=>{
+//   try {
+//     await Booking.findByIdAndDelete(req.params.id);
+//     res.status(200).json("Booking has been deleted.");
+//   } catch (err) {
+//     next(err);
+//   }
+// }
+// export const getBooking = async (req,res,next)=>{
+//   try {
+//     const Booking = await Booking.findById(req.params.id);
+//     res.status(200).json(Booking);
+//   } catch (err) {
+//     next(err);
+//   }
+// }
+// export const getBookings = async (req,res,next)=>{
+//   try {
+//     const Bookings = await Booking.find();
+//     res.status(200).json(Bookings);
+//   } catch (err) {
+//     next(err);
+//   }
+// }
