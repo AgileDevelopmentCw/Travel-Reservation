76bc89011e972ca9c1661044f13c9e94205f2f91
Sankalpana8 <thushani.sankalpana@gmail.com>
2023-10-14 13:16:12 +0530

    update

--- api/controllers/customer.js
+++ api/controllers/customer.js
@@ -21,57 +21,57 @@ export const createCustomer = async (req, res, next) => {
   }
 };
 
-// export const updateCustomer = async (req, res, next) => {
+export const updateCustomer = async (req, res, next) => {
+  try {
+    const updatedCustomer = await Customer.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedCustomer);
+  } catch (err) {
+    next(err);
+  }
+};
+export const updateCustomerAvailability = async (req, res, next) => {
+  try {
+    await Customer.updateOne(
+      { "CustomerNumbers._id": req.params.id },
+      {
+        $push: {
+          "CustomerNumbers.$.unavailableDates": req.body.dates
+        },
+      }
+    );
+    res.status(200).json("Customer status has been updated.");
+  } catch (err) {
+    next(err);
+  }
+};
+export const deleteCustomer = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  try {
+    await Customer.findByIdAndDelete(req.params.id);
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $pull: { Customers: req.params.id },
+      });
+    } catch (err) {
+      next(err);
+    }
+    res.status(200).json("Customer has been deleted.");
+  } catch (err) {
+    next(err);
+  }
+};
+// export const getCustomer = async (req, res, next) => {
 //   try {
-//     const updatedCustomer = await Customer.findByIdAndUpdate(
-//       req.params.id,
-//       { $set: req.body },
-//       { new: true }
-//     );
-//     res.status(200).json(updatedCustomer);
+//     const Customer = await Customer.findById(req.params.id);
+//     res.status(200).json(Customer);
 //   } catch (err) {
 //     next(err);
 //   }
 // };
-// export const updateCustomerAvailability = async (req, res, next) => {
-//   try {
-//     await Customer.updateOne(
-//       { "CustomerNumbers._id": req.params.id },
-//       {
-//         $push: {
-//           "CustomerNumbers.$.unavailableDates": req.body.dates
-//         },
-//       }
-//     );
-//     res.status(200).json("Customer status has been updated.");
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-// export const deleteCustomer = async (req, res, next) => {
-//   const hotelId = req.params.hotelid;
-//   try {
-//     await Customer.findByIdAndDelete(req.params.id);
-//     try {
-//       await Hotel.findByIdAndUpdate(hotelId, {
-//         $pull: { Customers: req.params.id },
-//       });
-//     } catch (err) {
-//       next(err);
-//     }
-//     res.status(200).json("Customer has been deleted.");
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-// // export const getCustomer = async (req, res, next) => {
-// //   try {
-// //     const Customer = await Customer.findById(req.params.id);
-// //     res.status(200).json(Customer);
-// //   } catch (err) {
-// //     next(err);
-// //   }
-// // };
 export const getCustomers = async (req, res, next) => {
   try {
     const Customers = await Customer.find();
